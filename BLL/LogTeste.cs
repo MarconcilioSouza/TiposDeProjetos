1b4378b04a928fe208cc7eb3c426008c484223a3
marconcilio das virgens <marconcilio@hotmail.com>
2018-11-24 12:51:10 -0200

    https://github.com/MarconcilioSouza/TiposDeProjetos.git

---
+++ BLL/LogTeste.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class LogTeste : ServiceBase
+    {
+        public void Logs()
+        {
+            Log.Information("Teste werwerewrwerewrwer");
+
+            LoggerExtensions.Information(Log, "Teste sdargverhtfgh");
+        }
+    }
+}
---
+++ XamForms/XamForms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamForms.Models;
+
+namespace XamForms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
